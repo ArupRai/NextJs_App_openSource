53c5ffb4d70e464ae1888c44390fad848af1d06b
praishere <prabhat.ece048@bppimt.ac.in>
2023-10-13 14:08:50 +0530

    Create form function not working

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,8 @@ import { GraphQLClient } from 'graphql-request'
 
 const API_URL = process.env.NEXT_PUBLIC_AWS_API_URL as string | null | undefined
 
+if (!API_URL)
+  throw new Error('process.env.NEXT_PUBLIC_AWS_API_URL is undefined')
 
 const isClient = typeof window !== 'undefined'
 

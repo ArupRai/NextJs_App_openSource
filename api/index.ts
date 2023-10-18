58a0afc38405b25fe0f0e84e71c62e5179351291
Sumit Kumar <106050212+sumit957602@users.noreply.github.com>
2023-10-18 19:31:16 +0530

    update

--- api/index.ts
+++ api/index.ts
@@ -2,9 +2,6 @@ import { GraphQLClient } from 'graphql-request'
 
 const API_URL = process.env.NEXT_PUBLIC_AWS_API_URL as string | null | undefined
 
-if (!API_URL)
-  throw new Error('process.env.NEXT_PUBLIC_AWS_API_URL is undefined')
-
 const isClient = typeof window !== 'undefined'
 
 export const graphqlClient = new GraphQLClient(`${API_URL}/graphql`, {

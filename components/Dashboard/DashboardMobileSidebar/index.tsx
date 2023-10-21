fb5ecede2e282a077186bbbd59c7a2f5eaabc7b3
praishere <prabhat.ece048@bppimt.ac.in>
2023-10-21 11:24:43 +0530

    add new component in form preview

--- components/Dashboard/DashboardMobileSidebar/index.tsx
+++ components/Dashboard/DashboardMobileSidebar/index.tsx
@@ -1,23 +1,25 @@
 import { Menu } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef } from 'react'
 
 import DashboardSidebar from '@/components/Dashboard/DashboardSidebar'
-import { Button } from '@/components/ui/button'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 
 const DashboardMobileSidebar: React.FC = () => {
-  const [isMounted, setIsMounted] = useState(false)
+  const ref = useRef(false)
 
-  useEffect(() => setIsMounted(true), [])
+  useEffect(() => {
+    ref.current = true
+    return () => {
+      ref.current = false
+    }
+  }, [])
 
-  if (!isMounted) return null
+  if (!ref.current) return null
 
   return (
     <Sheet>
-      <SheetTrigger>
-        <Button variant="ghost" size="icon" className="md:hidden">
-          <Menu />
-        </Button>
+      <SheetTrigger className="inline-flex h-10 w-10 items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 md:hidden">
+        <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
         <DashboardSidebar />
---
+++ components/Dashboard/FormPreview.tsx
@@ -0,0 +1,47 @@
+import { Laptop, Smartphone } from 'lucide-react'
+import { useState } from 'react'
+
+import { cn } from '@/lib/utils'
+import ReviewForm from './ReviewForm'
+
+const FormPreview: React.FC = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  return (
+    <div className="relative flex h-full w-full flex-col items-center">
+      <div className="mb-4 flex justify-center rounded-lg bg-gray-300 p-1">
+        <button
+          className={cn(
+            'offset_ring rounded-md p-1.5 transition-all',
+            isMobile && 'bg-gray-100'
+          )}
+          onClick={() => setIsMobile(true)}
+        >
+          <Smartphone />
+        </button>
+        <button
+          className={cn(
+            'offset_ring rounded-md p-1.5 transition-all',
+            !isMobile && 'bg-gray-100'
+          )}
+          onClick={() => setIsMobile(false)}
+        >
+          <Laptop />
+        </button>
+      </div>
+
+      <div
+        className={cn(
+          'relative flex h-[90%] w-full flex-none flex-col overflow-hidden rounded-md bg-gray-50 shadow-xl ring-4 ring-gray-800 duration-300',
+          {
+            'h-[628px] w-[330px] rounded-3xl': isMobile,
+          }
+        )}
+      >
+        <ReviewForm />
+      </div>
+    </div>
+  )
+}
+
+export default FormPreview
